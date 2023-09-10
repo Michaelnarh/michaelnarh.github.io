19f0b7d4a2531734fbd816eeb28fd7f0c2352eb3
michaelnarh <michaeltetteh62@gmail.com>
2023-09-10 05:28:51 +0000

    added tech of laravel

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,6 +21,7 @@ import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
+import laravel from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
@@ -88,4 +89,5 @@ export {
 	projectEE,
 	projectFF,
 	projectGG,
+	laravel,
 };
